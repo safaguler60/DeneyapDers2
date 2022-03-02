4ccaac5b76c0f1478a6e1594748be0c5f5c094af
Safa GÜLER <77540851+safaguler60@users.noreply.github.com>
2022-03-02 22:47:23 +0300

    Add files via upload

---
+++ deneyapders2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   int sayi;
+   cin>>sayi;
+   switch(sayi){
+   case 1:
+       cout<<"Pazartesi";
+       break;
+   case 2:
+    cout<<"Sali";
+    break;
+   case 3:
+    cout<<"Carsamba";
+    break;
+    default: cout<<"Bilgim Disinda";
+    break;
+
+   }
+
+
+
+}
+
+
